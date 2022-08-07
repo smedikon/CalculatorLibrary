f68053729b9b3cce24af9400bf9553f4a36759bd
smedikon <sandeep.medikonda@ansys.com>
2022-08-06 23:48:30 -0700

    multiply function added

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def miltiply(first_term, second_term):
+    return first_term * second_term
